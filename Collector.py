c8b7c400a1a26e5c017dc7285b43db3c09ef95b4
abdullah-mobin <abdullahmobin1@yahoo.com>
2023-08-26 23:42:05 +0600

    time and value updated

--- Collector.py
+++ Collector.py
@@ -8,7 +8,7 @@ from App import *
 app_usage = {}
 
 print("loop 1 start")
-while START <= END:
+while START <= END*60:
     active_window = gw.getActiveWindow()
     if active_window is not None:
 
@@ -19,11 +19,10 @@ while START <= END:
 
         app_usage.setdefault(app_name, []).append(current_time)
 
-    if datetime.datetime.now().second == 30:
+    if datetime.datetime.now().second == 10:
         with open('app_usage_data.txt', 'w') as f:
             for app_name, timestamps in app_usage.items():
                 f.write(f"{app_name}: {', '.join(str(ts) for ts in timestamps)}\n")
 
-    time.sleep(60)
-    START += 1
-    print(f"finish {START}")  # check if running well
+    time.sleep(10)
+    START += 10
